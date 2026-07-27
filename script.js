5be3b3ee09ced3635192a372c88c8b0c820c7fda
madasaleh188-ui <madasaleh.188@gmail.com>
2026-07-28 02:29:45 +0300

    Remove Firebase imports and configuration comments
    Removed Firebase imports and related comments.

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 // 1. FIREBASE CONFIGURATION
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCzTs_zw28wkHij4Jj9-EEW3XOpQ5si2yc",
